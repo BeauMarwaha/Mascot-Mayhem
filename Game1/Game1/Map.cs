3a6d6db00c77fe2be7093a4d6ed297800527d3bd
Beau Marwaha <beaumarwaha@gmail.com>
2016-03-04 11:08:59 -0500

    Added Map Class
    Created the Map class and added some starter code

---
+++ Game1/Game1/Map.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Author: Beau Marwaha
+//Purpose: Represents a full map to be used in game
+namespace Game1
+{
+    class Map
+    {
+        //attributes
+        private string name;
+        //private MapTile[] tiles; //needs maptile class to be coded
+
+        //param constructor
+        public Map(string nm) //add MapTile array once its coded
+        {
+            name = nm;
+        }
+
+        //getter for name
+        public string Name
+        {
+            get { return name; }
+        }
+
+        //add getter for MapTiles
+
+        //add PossibleMoves method
+    }
+}
