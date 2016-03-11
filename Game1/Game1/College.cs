f2c1d936f1323ca754b0378f3e5827b11ba8f1c1
Jared Miller <jem7457@g.rit.edu>
2016-03-11 15:19:00 -0500

    Added a College class as discussed. One thing I noticed is that instead of a default constructor I think it needs a parameterized one.

---
+++ Game1/Game1/College.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1
+{
+    class College
+    {
+        // attributes
+        // array for units to be loaded
+        Unit[] units;
+
+        // mascot unit to load
+        Mascot masc;
+
+        // name for the college
+        string collName;
+
+        // default constructor // <- CHANGE I think this needs to be parameterized
+        public College()
+        {
+           // set name of college
+        }
+
+        // method to load in the un units
+        public void Load()
+        {
+            // code here
+        }
+
+         
+    }
+}
